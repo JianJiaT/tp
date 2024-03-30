9e661a2ebd5ae860ac8de437a776b32c1a48e21f
STeng618 <shantengyap@gmail.com>
2024-03-30 17:52:14 +0800

    Test AddExpenseToEventParser

---
+++ src/test/java/brokeculator/parser/AddExpenseToEventParserTest.java
@@ -0,0 +1,48 @@
+package brokeculator.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import brokeculator.command.AddExpenseToEventCommand;
+import brokeculator.command.Command;
+import brokeculator.command.InvalidCommand;
+
+import org.junit.jupiter.api.Test;
+
+class AddExpenseToEventParserTest {
+
+    @Test
+    void parseInput_validInput_addExpenseToEventCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1 /evi 2");
+        assertInstanceOf(AddExpenseToEventCommand.class, command);
+    }
+
+    @Test
+    void parseInput_missingIndex_invalidCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1 /evi");
+        assertInstanceOf(InvalidCommand.class, command);
+    }
+
+    @Test
+    void parseInput_nonIntegerIndex_invalidCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1 /evi a");
+        assertInstanceOf(InvalidCommand.class, command);
+    }
+
+    @Test 
+    void parseInput_missingKeyword_invalidCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1");
+        assertInstanceOf(InvalidCommand.class, command);
+    }
+
+    @Test
+    void parseInput_tooManyArguments_invalidCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1 /evi 2 /exi 3");
+        assertInstanceOf(InvalidCommand.class, command);
+    }
+
+    @Test
+    void parseInput_nanIndex_invalidCommand() {
+        Command command = AddExpenseToEventParser.parseInput("addExpenseToEvent /exi 1 /evi NaN");
+        assertInstanceOf(InvalidCommand.class, command);
+    }
+}
