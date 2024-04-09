07482ffdf0f1ade5f82063f45fa12392c021ae42
JianJiaT <tjjtjn@gmail.com>
2024-04-09 21:28:04 +0800

    Add new error message in enum class for incorrect delete command format

--- src/main/java/brokeculator/enumerators/CommandErrorMessages.java
+++ src/main/java/brokeculator/enumerators/CommandErrorMessages.java
@@ -4,6 +4,8 @@ public enum CommandErrorMessages {
     INVALID_EXIT_COMMAND("Invalid command. Please enter 'exit' to exit the program."),
     INVALID_LIST_COMMAND("Invalid input format for list command. Please use the format:" + System.lineSeparator()
             + "\tlist /a [optional] <amount to list>"),
+    INVALID_DELETE_COMMAND("Invalid input format for delete command. Please use the format:" + System.lineSeparator()
+            + "\tdelete /i <index>"),
     INVALID_ADD_COMMAND("Invalid input format for add command. Please use the format:" + System.lineSeparator()
             + "\tadd /n <description> /d <date> /a <amount> /c [optional] <category>");
 
