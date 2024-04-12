3b9f9caba1a897d4acdaaa02b19baece903b6456
yzs <yeozhishen@gmail.com>
2024-04-12 19:01:32 +0800

    Remove CommandEnumerator class Unused class, therefore, removing it to avoid clutter

--- src/main/java/brokeculator/parser/ExitParser.java
+++ src/main/java/brokeculator/parser/ExitParser.java
@@ -2,14 +2,14 @@ package brokeculator.parser;
 
 import brokeculator.command.Command;
 import brokeculator.command.ExitCommand;
-import brokeculator.enumerators.CommandsEnumerator;
 import brokeculator.command.InvalidCommand;
 
 public class ExitParser {
+    private static final String EXIT_COMMAND = "exit";
     private static final String INVALID_EXIT_COMMAND = "Invalid command. Please enter 'exit' to exit the program.";
     public static Command parseInput(String userInput) {
 
-        if (userInput.equals(CommandsEnumerator.EXIT_COMMAND.getString())) {
+        if (userInput.equals(EXIT_COMMAND)) {
             return new ExitCommand();
         } else {
             return new InvalidCommand(INVALID_EXIT_COMMAND);
